aed8046a09bf458ab42fff617d211ee4f11ca4d1
Adrien Viviani <adrien.viv@gmail.com>
2019-04-03 14:45:28 +0800

    seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,14 @@
+Task.destroy_all
 
+10.times do
+  Task.create(
+    name: Faker::Restaurant.name,
+    address:Faker::Restaurant.description,
+    description: Faker::Address.street_address,
+    price_per_night: Random.rand(35...150),
+    number_of_guests: Random.rand(8)
+    )
+end
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
